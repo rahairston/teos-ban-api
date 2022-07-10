123025e8f61502fa8f9f1e460d411d297680254b
rahairston <rahairston2014@verizon.net>
2022-07-10 13:27:39 -0500

    beginning redis

---
+++ src/main/java/com/teosgame/ban/banapi/config/RedisConfig.java
@@ -0,0 +1,30 @@
+package com.teosgame.ban.banapi.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+
+@Configuration
+public class RedisConfig {
+
+    @Value("${ban.teo.redis.host:localhost}")
+    private String host;
+
+    @Bean
+    JedisConnectionFactory jedisConnectionFactory() {
+        JedisConnectionFactory jedisConFactory = new JedisConnectionFactory(
+            new RedisStandaloneConfiguration(host)
+        );
+        return jedisConFactory;
+    }
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate() {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(jedisConnectionFactory());
+        return template;
+    }
+}
